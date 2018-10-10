94903f8d5d49a7160fc1f38f0e2ace136df46fff
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-10 06:23:51 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,12 @@ class String
   end
 
   def count_sentences
-
+    counter = 0
+    array = self.split
+    array.each do |element|
+      if sentence?(element) || question?(element) || exclamation?(element)
+        counter += 1
+      end
+    end
   end
 end
